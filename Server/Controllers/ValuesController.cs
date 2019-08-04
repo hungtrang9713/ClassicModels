32e95961a62808a32bfcd8f69c2ece6bca9e9d8b
hungtrang <hungtrang9713@gmail.com>
2019-08-04 12:53:19 +0700

    Thủ thay đổi một file

--- Server/Controllers/ValuesController.cs
+++ Server/Controllers/ValuesController.cs
@@ -21,7 +21,7 @@ namespace Server.Controllers
         [HttpGet("{id}")]
         public ActionResult<string> Get(int id)
         {
-            return "value";
+            return "value"; 
         }
 
         // POST api/values
